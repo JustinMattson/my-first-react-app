4a0aaac6d4fb3e3c5de7ffd63663a9a9f178a7c5
Justin Mattson <justin.mattson@outlook.com>
2020-08-17 18:33:05 -0600

    Updates

---
+++ precache-manifest.89a5ecdac8e2d476780ebbaffc7c9e99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965890eeb466cda0c570dc03a2cd3c5e",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "8b1691c5a9afd6c5384e",
+    "url": "/my-first-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a469888bf3c34e6f20ff",
+    "url": "/my-first-react-app/static/js/2.12b8cc34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.12b8cc34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1691c5a9afd6c5384e",
+    "url": "/my-first-react-app/static/js/main.8ab997fc.chunk.js"
+  },
+  {
+    "revision": "9fa5d64e5b1bb9a77b80",
+    "url": "/my-first-react-app/static/js/runtime-main.c3e2ea89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
